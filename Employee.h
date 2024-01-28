da3a719144d52e26fcceb5d3d49d0e62daae9982
WOJTASK1 <wojtek03.lep@gmail.com>
2024-01-28 21:03:20 +0100

    added methods

--- Employee.h
+++ Employee.h
@@ -2,3 +2,14 @@
 #include <string>
 #include <vector>
 using namespace std;
+class Employee {
+public:
+    string name;
+    string position;
+    int age;
+    Employee(string n,string p, int a) : name(n),position(p), age(a) {}
+    friend ostream &operator<<(ostream &s, const Employee &e) {
+		s<<"Name:"<<e.name<<" ,position:"<<e.position<<" ,Age:"<<e.age;
+	return s;
+	}
+};
