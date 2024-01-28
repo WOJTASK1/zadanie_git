72eda196d766d713021dd80f49a9cd33a13020a7
WOJTASK1 <wojtek03.lep@gmail.com>
2024-01-28 21:43:48 +0100

    made class

--- Map.h
+++ Map.h
@@ -2,3 +2,14 @@
 #include <string>
 #include <vector>
 using namespace std;
+
+// Wzorzec klasy Map
+template <typename KeyType, typename ObjectType>
+class Map {
+private:
+    vector<KeyType> keys;
+    vector<ObjectType> objects;
+
+public:
+   
+};
