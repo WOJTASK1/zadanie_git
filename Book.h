00bb9350719feac7c0dc2c99fb0af8d557ee6751
WOJTASK1 <wojtek03.lep@gmail.com>
2024-01-28 21:10:22 +0100

    added << method

--- Book.h
+++ Book.h
@@ -8,6 +8,11 @@ class Book{
 	string author;
 	string category;
 	bool available; //true-available false-borrowed
-	unsigned int pages;	 
+	unsigned int pages;
+	Book(string au, string c, bool a, unsigned int p):author(au),category(c), available(a),pages(p){}
+	friend ostream &operator<<(ostream &s, const Book &b) {
+		s<<"Author:"<<b.author<<" ,category:"<<b.category<<" ,number of pages:"<<b.pages<<" ,available: "<<b.available;
+	return s;
+	}	 
 	
 };
