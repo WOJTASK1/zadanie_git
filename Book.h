7151cfab29aa45501dbff531de1b515a0b898c4a
WOJTASK1 <wojtek03.lep@gmail.com>
2024-01-28 21:09:35 +0100

    added fields to class

--- Book.h
+++ Book.h
@@ -2,3 +2,12 @@
 #include <string>
 #include <vector>
 using namespace std;
+
+class Book{
+	public:
+	string author;
+	string category;
+	bool available; //true-available false-borrowed
+	unsigned int pages;	 
+	
+};
